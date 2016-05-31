05b14797f25c594e09b4387f25daabac3d189b35
tharindu99 <madushanka.best@gmail.com>
2016-05-31 09:58:24 +0530

    added hibernate to the project

--- KarshaCEP/src/com/lsf/cep/DBopenConnection.java
+++ KarshaCEP/src/com/lsf/cep/DBopenConnection.java
@@ -7,15 +7,15 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.lsf.entity.Stock;
+
 public class DBopenConnection {
 	public void DBopen_me() {
 		SessionFactory SFact = new Configuration().configure().buildSessionFactory();
 		Session session = SFact.openSession();
 		org.hibernate.Transaction tx = session.beginTransaction();
 		
-		String query = "SELECT date as AllDates,PRC,`Pseudo-PRC` FROM top10 WHERE PERMNO = 66800";
-		SQLQuery q = session.createSQLQuery(query);	
-		List data = q.list();
-		System.out.println(data);
+		List<Stock> stk = session.createSQLQuery("SELECT * FROM stock").addEntity(Stock.class).list();
+		System.out.println(stk.get(0).getPrc());
 	}
 }
